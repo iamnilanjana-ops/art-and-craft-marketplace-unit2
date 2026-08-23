6202b8ccd33afc94be2e6a1cbee38d294f0196ec
iamnilanjana-ops <iamnilanjana@gmail.com>
2026-08-22 20:14:05 -0500

    Set up Spring Boot project and MySQL database configuration

---
+++ src/main/java/org/launchcode/artcraftmarketplace/ArtcraftmarketplaceApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.artcraftmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtcraftmarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtcraftmarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/artcraftmarketplace/ArtcraftmarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.artcraftmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtcraftmarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
